1346025aa6093bb666fccf9a46297d1a101ef114
aklreaxmer <amkramer2010@yahoo.com>
2014-10-28 12:38:18 -0700

    Remove unused code

--- src/Printer.java
+++ src/Printer.java
@@ -1,13 +1,6 @@
 
 public class Printer {
-	
-	PrintingPeep peep;
-	String toPrint;
-	public Printer()
-	{
-		this.peep = peep;
-		this.toPrint = toPrint;
-	}
+
 	public void print(PrintingPeep peep, int numJobs)
 	{
 		System.out.println("Print job from " + peep.name() + " - ID: " + peep.ID() + " - Job #" + numJobs);
